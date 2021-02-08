10c5e1713ec34f0f28513d1f4832da4234244483
Maira Acevedo <m.acevedo53@yahoo.com>
2021-02-07 22:44:46 -0800

    minor update to server.js file

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/GoogleBooks"),
 
 // Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  console.log(`App running on PORT ${PORT}!`);
 });
